77840f79bd503a0e281260c8aff7b82698d1b887
Gagandeep07_Kaur <gagandeep07kaur@gmail.com>
2020-09-13 16:39:23 +0530

    added multiple reducers

---
+++ redux_basics/multiple_reducers.js
@@ -0,0 +1,66 @@
+// const {redux, createStore, combineReducers } = require('redux'); To import all in single line
+
+const redux = require('redux');
+const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
+
+const initialStateBooks = {     //initialise the values of books
+    numberOfBooks: 10,    
+}
+
+const initialStatePens = {     //initialise the values of pens
+    numberOfPens: 15
+}
+
+
+function buyBook() { // define buyBook action, and return type and other info
+    return {
+        // action
+        type: "Buy_Book",
+        payload: "My first action"
+    }
+}
+
+function buyPen() {  // define buyPen action, and return type and other info
+    return {
+        // action
+        type: "Buy_Pen",
+        payload: "My second action"
+    }
+}
+
+
+// In reducer (prevState, action) => newState
+
+const booksReducer = (state = initialStateBooks, action) => { 
+    switch(action.type) {
+        case "Buy_Book": return{
+            ...state,               //to clone state value
+            numberOfBooks: state.numberOfBooks - 1
+        }   
+
+        default: return state;
+    }
+
+}
+
+const pensReducer = (state = initialStatePens, action) => { 
+    switch(action.type) {
+        case "Buy_Pen": return{
+            ...state,               //to clone state value
+            numberOfPens: state.numberOfPens - 2
+        }   
+
+        default: return state;
+    }
+
+}
+
+const store = createStore(pensReducer); //created store and passing reducer function into it
+console.log("Initial State", store.getState());
+const unsubscribe = store.subscribe(()=> {console.log('Updated state value', store.getState())});
+store.dispatch(buyBook());
+store.dispatch(buyBook());
+store.dispatch(buyPen());
+store.dispatch(buyPen());
+unsubscribe();
