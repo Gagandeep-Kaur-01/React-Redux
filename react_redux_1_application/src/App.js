b6d5a438ff62652f763741c7e0311d085029f4fc
Gagandeep07_Kaur <gagandeep07kaur@gmail.com>
2020-09-14 09:13:02 +0530

    imported provider and store (passed it as props) in App.js

--- react_redux_1_application/src/App.js
+++ react_redux_1_application/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import {Provider} from 'react-redux';
 import BookContainer from './components/BookContainer';
+import store from './redux/store'
 import './App.css';
 
 function App() {
   return (
-   
+    <Provider store = {store}>
     <div className="App">
-      <BookContainer/>          
+      <BookContainer/>    
     </div>
-    
+    </Provider>
   );
 }
 
